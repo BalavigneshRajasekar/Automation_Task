576616d63f77efffcdee0065a53924ecb33b1b68
Balavignesh rajasekar <vigneshvickybsc1999@gmail.com>
2025-07-23 23:35:25 +0530

    Created new login for facebook

--- src/main/java/learnAutomation/searchGoogle.java
+++ src/main/java/learnAutomation/searchGoogle.java
@@ -14,12 +14,22 @@ public class searchGoogle {
 		WebDriverManager.chromedriver().setup();
 		
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		WebElement searchBar =driver.findElement(By.xpath("//textarea[@name=\"q\"]"));
-		searchBar.sendKeys("facebook.com");
-		searchBar.submit();
+		driver.get("https://www.facebook.com");
+		WebElement email = driver.findElement(By.xpath("//input[@id=\"email\"]"));
+		WebElement password = driver.findElement(By.xpath("//input[@id=\"pass\"]"));
+		WebElement eyeIcon = driver.findElement(By.xpath("//div[@class=\"_9lsa\"]"));
+		WebElement	logInButton = driver.findElement(By.xpath("//button[@name=\"login\"]"));
 		
-
+		
+		email.sendKeys("kingbala62@gmail.com");
+		password.sendKeys("fb.com");
+	    
+		try {
+			Thread.sleep(2000);
+		}catch(Exception e) {}
+		
+       eyeIcon.click();
+       logInButton.click();
 	}
 
 }
