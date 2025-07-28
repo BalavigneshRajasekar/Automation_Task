7b2c9c54a8d4761d9ee0a36c001df00fd03f55c8
Balavignesh rajasekar <vigneshvickybsc1999@gmail.com>
2025-07-28 22:52:01 +0530

    Sortable test case created

--- src/main/java/learnAutomation/ActionClass.java
+++ src/main/java/learnAutomation/ActionClass.java
@@ -1,5 +1,8 @@
 package learnAutomation;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,6 +21,7 @@ public class ActionClass {
 		driver = new ChromeDriver();
 		obj = new Actions(driver);
 		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().fullscreen();
 	
 		
 	}
@@ -35,17 +39,54 @@ public void dragAndDrop() {
 public void resizable() {
 	driver.findElement(By.xpath("//a[text()=\"Resizable\"]")).click();
 	WebElement	Frames = driver.findElement(By.xpath("//div[@id=\"content\"]//descendant::iframe"));
-	System.out.println("switched");
 	driver.switchTo().frame(Frames);
-	System.out.println("switched");
 	WebElement Icon = driver.findElement(By.xpath("//div[@id=\"resizable\"]/div[3]"));
 	obj.dragAndDropBy(Icon, 300, 100).perform();
-	
+	driver.switchTo().defaultContent();
 	
 	
 }	
 		
 	
+public void scrollAndSelect() {
+	driver.findElement(By.xpath("//a[text()=\"Selectable\"]")).click();
+	WebElement	Frames = driver.findElement(By.xpath("//div[@id=\"content\"]//descendant::iframe"));
+	driver.switchTo().frame(Frames);
+	WebElement scrollable=driver.findElement(By.xpath("//ol[@id=\"selectable\"]//li[3]"));
+	WebElement stop = driver.findElement(By.xpath("//ol[@id=\"selectable\"]//li[7]"));
+	obj.dragAndDrop(scrollable, stop).perform();
+	driver.switchTo().defaultContent();
+	
+}
+
+public void sortable() {
+	driver.findElement(By.xpath("//a[text()=\"Sortable\"]")).click();
+	WebElement	Frames = driver.findElement(By.xpath("//div[@id=\"content\"]//descendant::iframe"));
+	driver.switchTo().frame(Frames);
+	List <WebElement> item = driver.findElements(By.className("ui-state-default"));
+	System.out.println(item.size());
+	
+   for(int i=0 ;i<item.size();i++) {
+	   WebElement current = driver.findElements(By.cssSelector("#sortable li")).get(i); // refresh DOM
+       WebElement target = driver.findElements(By.cssSelector("#sortable li")).get(6);  // 7th item (index 6)
+
+       if (i != 6) { // Don't drag item 7 to itself
+           obj.clickAndHold(current)
+               .moveToElement(target)
+               .moveByOffset(0, 10) // fine-tune position
+               .release()
+               .perform();
+       }
+	   
+	    try {
+	    	Thread.sleep(1500);
+	    	
+	    }catch(Exception io) {}
+	
+     }
+   System.out.println("sorted");
+	
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -53,6 +94,8 @@ public void resizable() {
 		obj1.setup();
 		obj1.dragAndDrop();
 		obj1.resizable();
+		obj1.scrollAndSelect();
+		obj1.sortable();
 		
          }
 
