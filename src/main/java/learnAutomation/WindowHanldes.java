e39e966dddfcbee8623dae5c1f0e800d908a5e09
Balavignesh rajasekar <vigneshvickybsc1999@gmail.com>
2025-07-30 23:23:20 +0530

    NEw one created

---
+++ src/main/java/learnAutomation/WindowHanldes.java
@@ -0,0 +1,14 @@
+package learnAutomation;
+
+
+public class WindowHanldes {
+	 
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         WindowHanldes obj = new WindowHanldes();
+     
+         }
+
+}
