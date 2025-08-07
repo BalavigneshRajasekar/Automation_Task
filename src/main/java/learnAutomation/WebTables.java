e7a3d95d0b56c1da5d9d45df44a25a85ee1c3afb
Balavignesh rajasekar <vigneshvickybsc1999@gmail.com>
2025-08-07 23:42:08 +0530

    Table handles

--- src/main/java/learnAutomation/WebTables.java
+++ src/main/java/learnAutomation/WebTables.java
@@ -1,17 +1,13 @@
 package learnAutomation;
 
-import java.time.Duration;
-import java.util.Iterator;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -24,31 +20,25 @@ public class WebTables {
 	public void setup() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("https://www.asx.com.au/markets/trade-our-cash-market/equity-market-prices");
-		WebDriverWait explicit = new WebDriverWait(driver,Duration.ofSeconds(10));
-		explicit.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id=\"onetrust-accept-btn-handler\"]"))).click();
+		driver.get("https://www.tutorialspoint.com/selenium/practice/webtables.php");
+		
 	}
 	
 	
 	
 	public void dynamicTable() {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-		// Wait until at least one row is present
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
-		    By.xpath("//*[@id=\"home_top_twenty\"]/div/div[1]/div/div[1]/table/tr")
-		));
+		
 		
 	List<WebElement>tableRow = driver.findElements(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr"));
 	
-	List<WebElement> tableCol = driver.findElements(By.xpath("//*[@id=\"home_top_twenty\"]/div/div[1]/div/div[1]/table/tr[2]/td"));
+	List<WebElement> tableCol = driver.findElements(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr[1]/td"));
 	
 	System.out.println(tableRow.size());
-	for(int i= 2; i<=tableRow.size();i++) {
+	for(int i= 1; i<=tableRow.size();i++) {
 		
 		for(int j=1;j<=tableCol.size();j++) {
 			
-			System.out.println(driver.findElement(By.xpath("//*[@id=\"home_top_twenty\"]/div/div[1]/div/div[1]/table/tr["+ i +"]/td["+ j +"]")).getText());
+			System.out.print(driver.findElement(By.xpath("/html/body/main/div/div/div[2]/form/div[2]/table/tbody/tr["+ i +"]/td["+ j +"]")).getText()+" " );
 		}
 		System.out.println();
 		
